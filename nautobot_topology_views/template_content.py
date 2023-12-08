b3bf46079970d9f8fd8ea2466e610072a852ed50
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-12-08 12:35:50 -0600

    style: 🐛 Change names to be Nautobot

--- nautobot_topology_views/template_content.py
+++ nautobot_topology_views/template_content.py
@@ -2,18 +2,21 @@ from nautobot.extras.plugins import PluginTemplateExtension
 from django.conf import settings
 from packaging import version
 
-NETBOX_CURRENT_VERSION = version.parse(settings.VERSION)
+NAUTOBOT_CURRENT_VERSION = version.parse(settings.VERSION)
 
 
 class SiteButtons(PluginTemplateExtension):
-    model = 'dcim.site'
+    model = "dcim.site"
+
     def buttons(self):
-        return self.render('nautobot_topology_views/site_button.html')
+        return self.render("nautobot_topology_views/site_button.html")
+
 
 class LocationButtons(PluginTemplateExtension):
-    model = 'dcim.location'
+    model = "dcim.location"
+
     def buttons(self):
-        return self.render('nautobot_topology_views/location_button.html')
+        return self.render("nautobot_topology_views/location_button.html")
 
 
 template_extensions = [SiteButtons, LocationButtons]
