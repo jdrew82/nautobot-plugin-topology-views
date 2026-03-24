d6bd6cb5923104c8b81f074d805d4e92be7ea656
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2026-03-24 17:05:18 -0500

    fix: 🐛 Fix the Topology button on Locations

--- nautobot_topology_views/template_content.py
+++ nautobot_topology_views/template_content.py
@@ -3,29 +3,13 @@
 from nautobot.extras.plugins import TemplateExtension
 
 
-
-class LocationButtons(TemplateExtension):
+class LocationButtons(TemplateExtension):  # pylint: disable=abstract-method
     """Add topology view button to Location detail pages."""
 
     model = "dcim.location"
-
-    def detail_tabs(self):
-        return []
-
-    def full_width_page(self):
-        return ""
-
-    def left_page(self):
-        return ""
-
-    def list_buttons(self):
-        return ""
-
-    def right_page(self):
-        return ""
 
     def buttons(self):
         return self.render("nautobot_topology_views/location_button.html")
 
 
-template_extensions = [SiteButtons, LocationButtons]
+template_extensions = [LocationButtons]
